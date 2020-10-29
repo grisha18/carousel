58c88552f1de7b4ce751c739394591f646c344f6
maks <a@a.a>
2020-10-29 11:25:47 +0300

    horizontal

--- script.js
+++ script.js
@@ -3,16 +3,20 @@ let active = 2;
 let list = document.querySelectorAll('.carousel .item');
 const listLength = list.length;
 
-
+const ul = document.querySelector("ul");
 
 const nextArrow = document.querySelector('.next-arrow');
 nextArrow.addEventListener('click', ()=>{
     
     const next =  (active + 1 ) % listLength;
+    const position = next * 400;
 
-    console.log(`next is ${next} `)
-    list[active].classList.remove('active');
-    list[next].classList.add('active');
+    console.log(`${position}px;`)
+    ul.style.left = `-${position}px`
+
+    // console.log(`next is ${next} `)
+    // list[active].classList.remove('active');
+    // list[next].classList.add('active');
 
     active = next;
 
